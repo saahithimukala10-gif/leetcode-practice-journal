86d20757ada5b65dfa9e14d3b0d63ae37dc21a7e
Saahithi <saahithimukala10@gmail.com>
2026-07-23 02:32:57 +0530

    Add Supabase setup and authentication UI

---
+++ js/supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://wlcuuciyjawgsnaozzfy.supabase.co";
+const SUPABASE_KEY = "sb_publishable_l8hjqCn9nVr9-TzcGxPWgg_gn49uNM-";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
